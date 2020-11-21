e9f859891b8e9339a2ef5f345c2c06f6d98e953a
Alex Batsman <crashius@gmail.com>
2020-11-21 22:06:01 +0000

    add docker file

--- app/src/main/java/com/tl/pokemon/App.java
+++ app/src/main/java/com/tl/pokemon/App.java
@@ -1,14 +1,16 @@
 package com.tl.pokemon;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class App {
 
-    public String getGreeting() {
-        return "Hello World!";
-    }
+	public String getGreeting() {
+		return "Hello World!";
+	}
 
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+	public static void main(String[] args) {
+		port(8080);
+		get("/hello", (req, res) -> "Hello World");
+	}
 }
